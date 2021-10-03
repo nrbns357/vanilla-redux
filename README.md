ef9787b45007cc2f25648b89c3e1fd6054d0df1b
nrbns357 <gusgh357@gmail.com>
2021-10-03 12:16:34 +0900

    upDate: add (dispatch, createStore, subscribe)

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Vanilla Redux
 
 Learning Vanilla-Redux and React-Redux
+
+****새로 배운 내용****<br/><br/><br/>
+
+
+**#dispatch() 함수**  
+인자에 값을 전달하는 함수<br/><br/>
+
+**#예시**<br/>
+```
+const countModifier = (count = 0, action) => { //modify : 수정하다.
+      if(action.type === "add"){
+            return count +1;
+      }else if(action.type === "minus"){
+            return count -1;
+      }else
+      return count;
+};
+```
--- src/index.js
+++ src/index.js
@@ -4,22 +4,38 @@ const add = document.getElementById('add'),
       minus = document.getElementById('minus'),
       number = document.querySelector('span');
 
-const store = createStore();
-      let count = 0;
+      number.innerText= 0;
 
-const updateNumber = ()=>{
-  number.innerText=count;
+const countModifier = (count = 0, action) => { //modify : 수정하다.
+      if(action.type === "add"){
+            return count +1;
+      }else if(action.type === "minus"){
+            return count -1;
+      }else
+      return count;
+};
+
+const countStore = createStore(countModifier);
+      
+
+const onchange = () => {
+      number.innerText = countStore.getState();
 }
 
-const handleAdd= () => {
-  count+=99999990;
-  updateNumber();
+countStore.subscribe(onchange);
+
+
+const handleAdd = ()=>{
+      countStore.dispatch({type : 'add'});
 }
 
-const handleMinus = () => {
-  count-=19999999999;
-  updateNumber();
+const handleMinus = () =>{
+      countStore.dispatch({type : 'minus'})
 }
 
-add.addEventListener("click" ,handleAdd);
+
+add.addEventListener('click',handleAdd);
 minus.addEventListener('click',handleMinus);
+
+//countStore.dispatch({ type : "ADD"}); // countModifier 인자 중 action인자 값을 보내주는 함수 (dispatch)의 역할임
+//console.log(countStore.getState()); //countModifier의 리턴 값을 콘솔로 보여주는 함수
