144e7f6e6803eb9a7cdc402d9f08cb3da46c1805
nrbns357 <gusgh357@gmail.com>
2021-10-03 23:21:06 +0900

    upDate: add Redux file,Router file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../router/Home";
+import Detail from "../router/Detail";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Router>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/:id" exact component={Detail}></Route>
+    </Router>
+  );
 };
 
 export default App;
